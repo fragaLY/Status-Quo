e598570da5a048eb94fff90add688c9775d11be0
Vadzim Kavalkou <fragalymlg@gmail.com>
2017-04-07 21:15:05 +0300

    ClientDao
    cd

--- dao/src/main/java/sq/vk/dao/client/ClientDao.java
+++ dao/src/main/java/sq/vk/dao/client/ClientDao.java
@@ -1,22 +1,19 @@
 package sq.vk.dao.client;
 
-import org.springframework.cache.annotation.Cacheable;
-import sq.vk.domain.client.Client;
-
 import java.util.List;
+
+import sq.vk.domain.client.Client;
 
 /**
  * Created by Vadzim Kavalkou on 22.03.2017.
  */
 public interface ClientDao {
 
-    @Cacheable(value = "clientCache")
     List<Client> getAllClients();
 
-    @Cacheable(value = "clientCache")
     Client getClientByEmail(String email);
 
-    @Cacheable(value = "clientCache")
     Client getClientById(Integer id);
 
+    boolean saveClient(Client client);
 }
