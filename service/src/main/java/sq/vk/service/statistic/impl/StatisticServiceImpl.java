39071b86756ec99f69c4595350f30ee23eb06b40
Vadzim Kavalkou <fragalymlg@gmail.com>
2017-04-09 01:06:12 +0300

    Statistic features

--- service/src/main/java/sq/vk/service/statistic/impl/StatisticServiceImpl.java
+++ service/src/main/java/sq/vk/service/statistic/impl/StatisticServiceImpl.java
@@ -1,5 +1,7 @@
 package sq.vk.service.statistic.impl;
 
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import sq.vk.converter.statistic.StatisticConverter;
 import sq.vk.dao.statistic.StatisticDao;
+import sq.vk.domain.statistic.PokerRoomType;
 import sq.vk.domain.statistic.Statistic;
 import sq.vk.dto.statistic.StatisticDto;
 import sq.vk.service.statistic.StatisticService;
@@ -21,6 +24,8 @@ import java.util.stream.Collectors;
 public class StatisticServiceImpl implements StatisticService {
 
     private static final Logger LOG = LoggerFactory.getLogger(StatisticServiceImpl.class);
+
+    private static final String TOTAL_PROFIT_SELECTOR = "#mainplayergrid-row-0 > td:nth-child(9) > div";
 
     @Autowired
     private StatisticDao dao;
@@ -59,5 +64,22 @@ public class StatisticServiceImpl implements StatisticService {
         LOG.info("Starting to get statistic by name = [{}].", name);
 
         return converter.apply(dao.getItemByName(name));
+    }
+
+    @Override
+    @Transactional(readOnly = false)
+    public Double getTotalProfit(final String name, final PokerRoomType roomType) {
+
+        final String uri = new StringBuilder("https://www.sharkscope.com/#Player-Statistics//networks/")
+                .append("PokerStars")
+                .append("/players/")
+                .append(name)
+                .toString();
+
+        Document document = new Document(uri);
+
+        Elements select = document.select(TOTAL_PROFIT_SELECTOR);
+
+        return Double.valueOf(select.toString().substring(1));
     }
 }
--- src/main/java/sq/vk/controller/StatisticController.java
+++ src/main/java/sq/vk/controller/StatisticController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import sq.vk.domain.statistic.PokerRoomType;
 import sq.vk.dto.statistic.StatisticDto;
 import sq.vk.service.statistic.StatisticService;
 
@@ -30,22 +31,26 @@ public class StatisticController {
     @Autowired
     private StatisticService statisticService;
 
-    @GetMapping(value = "/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{room}/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public ResponseEntity<StatisticDto> getStatisticByName(@Valid @PathVariable("name") String name)
+    public ResponseEntity<StatisticDto> getStatisticOfPlayerInRoom(@Valid @PathVariable("room") String room, @Valid @PathVariable("name") String name)
             throws IOException {
 
         LOG.info("Get statistic by name: '{}'", name);
+        final long currentTime = now().atZone(EUROPE_MOSCOW).toInstant().toEpochMilli();
 
         HttpStatus httpStatus = HttpStatus.OK;
 
-        final long currentTime = now().atZone(EUROPE_MOSCOW).toInstant().toEpochMilli();
+        final PokerRoomType roomType = PokerRoomType.getRoomAsEnum(room.toUpperCase());
 
-        StatisticDto item = statisticService.getItemByName(name);
+        double profit = statisticService.getTotalProfit(name, roomType);
+
+        StatisticDto dto = new StatisticDto.Builder().setName(name).setProfit(profit).setPokerRoom(roomType).build();
 
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.setLastModified(currentTime);
 
-        return new ResponseEntity<>(item, responseHeaders, httpStatus);
+        return new ResponseEntity<>(dto, responseHeaders, httpStatus);
     }
+
 }
