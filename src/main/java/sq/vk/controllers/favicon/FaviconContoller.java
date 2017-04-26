cc2b39d1a0d91c1a918032b5d9cbb7d584b68d5a
Vadzim Kavalkou <fragalymlg@gmail.com>
2017-04-26 10:59:43 +0300

    1.Implement service logic. 2.Improve controllers (HATEOAS)

--- src/main/java/sq/vk/controllers/favicon/FaviconContoller.java
+++ src/main/java/sq/vk/controllers/favicon/FaviconContoller.java
@@ -1,19 +1,35 @@
 package sq.vk.controllers.favicon;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import sq.vk.controllers.exceptionhandlers.errordetails.ErrorDetails;
+import sq.vk.core.dto.statistic.StatisticDto;
 
 /**
  * Created by Vadzim_Kavalkou on 4/11/2017.
  */
 @Controller
+@Api(value = "favicon", description = "Favicon API")
 @RequestMapping("favicon.ico")
 public class FaviconContoller {
 
   private static final String FORWARD_FAVICON = "forward:/resources/images/favicon.ico";
 
   @GetMapping
+  @ApiOperation(value = "Retrieves favicon",
+      notes = "The path of favicon will be sent in the location response",
+      response = StatisticDto.class)
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "The path of favicon was retrieved", response = String.class),
+      @ApiResponse(code = 401, message = "Unauthorized client", response = ErrorDetails.class),
+      @ApiResponse(code = 403, message = "Access denied", response = ErrorDetails.class),
+      @ApiResponse(code = 404, message = "The path of favicon was not found", response = ErrorDetails.class),
+      @ApiResponse(code = 500, message = "Error getting the path of favicon", response = ErrorDetails.class)} )
   public String getFavicon() {
 
     return FORWARD_FAVICON;
